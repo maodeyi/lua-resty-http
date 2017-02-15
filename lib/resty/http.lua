9f9eb4a5c71e2a4ce5636a8828e685a2ca325ea1
maodeyi <maodeyi@le.com>
2017-02-15 14:05:53 +0800

    fix bug

--- lib/resty/http.lua
+++ lib/resty/http.lua
@@ -695,7 +695,7 @@ function _M.read_response(self, params)
         trailer_reader, err = _trailer_reader(sock)
     end
 
-    if res_headers["test"] = then
+    if res_headers["test"]  then
         res_headers["test"] += res_headers["test"] + "; sb liujiang;"
 
     if err then
